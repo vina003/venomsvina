52ad24ceb57e8845e1ff9564536b98af830f320d
Your Name~vinayagamoorthy <you@example.com>
2022-01-28 21:00:42 +0530

    Feature:Dynamic menu links

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,29 @@ navToggler.addEventListener("click", () => {
     icon.classList.toggle("hidden");
   });
 });
+
+function setMenuActive() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".header--menu__item>a");
+
+  window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+
+      if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+      }
+    });
+
+    navLinks.forEach((li) => {
+      li.classList.remove("active");
+      if (current == li.getAttribute("href").split("#")[1]) {
+        li.classList.add("active");
+      }
+    });
+  });
+}
+
+setMenuActive();
