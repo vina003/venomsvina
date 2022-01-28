f662ff3b27e52e422ddb6df707f7dda9081f9791
Your Name~vinayagamoorthy <you@example.com>
2022-01-28 21:34:05 +0530

    fix: menulinks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const navToggler = document.querySelector(".nav-toggler");
+const navLinks = document.querySelectorAll(".header--menu__item>a");
 
-navToggler.addEventListener("click", () => {
+navToggler.addEventListener("click", changeMenuState);
+
+function changeMenuState() {
   const headerMenu = document.querySelector("ul.header--menu");
   const navIcon = document.querySelectorAll(".navIcon");
 
@@ -10,11 +13,10 @@ navToggler.addEventListener("click", () => {
   navIcon.forEach((icon) => {
     icon.classList.toggle("hidden");
   });
-});
+}
 
 function setMenuActive() {
   const sections = document.querySelectorAll("section");
-  const navLinks = document.querySelectorAll(".header--menu__item>a");
 
   window.addEventListener("scroll", () => {
     let current = "";
@@ -36,4 +38,10 @@ function setMenuActive() {
   });
 }
 
+function onMenuClick() {
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", changeMenuState);
+  }
+}
 setMenuActive();
+onMenuClick();
